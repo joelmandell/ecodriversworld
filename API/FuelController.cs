68192d96a8694b3f044fdf52f3172974855dd847
Joel Mandell <joelmandell@gmail.com>
2013-06-14 18:24:33 +0300

    Removed unecessary variable and added CONSTANTS for UK and US volume and distance.

--- API/FuelController.cs
+++ API/FuelController.cs
@@ -9,14 +9,15 @@ namespace DataNissen.API
 {
     public class FuelController : ApiController
     {
+        //Constants of distances and volume, converted to ther respective value in kilometers and liters.
+        public const double ENGLISH_MILE = 1.6093472186944;
+        public const double ENGLISH_GALLON = 4.54609188;
+        public const double AMERICAN_GALLON = 3.78541178;
+
         //
         // GET /api/fuel
         public HttpResponseMessage Get(string metrics="EU", string sDistance="100km", double used=8.2)
         {
-            //Not really constants, but they are constants in the sense of
-            //that they are used when converting from Kilometers to Miles.
-            double distanceConst = 0; 
-            double fuelConst = 0;
             double calculatedAverage = 0;
             double distance = 0;
             string parseDistance = "";
@@ -35,27 +36,22 @@ namespace DataNissen.API
             //Convert founded number value in sDistance to double (because it might be with decimals).
             distance = Convert.ToDouble(parseDistance, System.Globalization.CultureInfo.InvariantCulture);
 
-            //Else-conditions if converting from Liters / KM.
+            //Else-conditions if converting from Liters / KM to US MPG(Miles per Gallon) or UK MPG.
             if (metrics.Equals("US"))
             {
-                distanceConst = 1.6093472186944; //1 KM is about 1.6 miles
-                fuelConst = 3.78541178; //1 Liter is about 3.78 Gallons.
-                calculatedAverage = (distance / distanceConst) / (used / fuelConst);
+                calculatedAverage = (distance / ENGLISH_MILE) / (used / AMERICAN_GALLON);
             }
             else if (metrics.Equals("UK"))
             {
-                distanceConst = 1.6093472186944; //1 KM is about 1.6 miles
-                fuelConst = 4.54609188; //1 Liter is about 3.78 Gallons.
-                calculatedAverage = (distance / distanceConst) / (used / fuelConst);
+                calculatedAverage = (distance / ENGLISH_MILE) / (used / ENGLISH_GALLON);
             }
             else if (metrics.Equals("EU"))
             {
-                distanceConst = 1;
-                fuelConst = 1;
                 calculatedAverage = (used / (distance / 100));
             }
 
-            //Using own HttpResponseMessage for future additions that might be added - so we can push multiply results to API requester.
+            //Using own HttpResponseMessage for future additions that might be added 
+            //- so we can push multiply results to API requester.
             return new HttpResponseMessage()
             {
                 Content = new StringContent(

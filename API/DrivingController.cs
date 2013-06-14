220f0321e507835a9cc1f4d1e6fb4dfc34a20e56
Joel Mandell <joelmandell@gmail.com>
2013-06-14 17:29:28 +0300

    Initial

---
+++ API/DrivingController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataNissen.API
+{
+    public class DrivingController : ApiController
+    {
+
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        //
+        // GET /api/driving
+        public HttpResponseMessage Get(string metrics="EU", string sDistance="100km", double usedfuel=8.2, string convertToMetrics="")
+        {
+
+            //Not really constants, but they are constants in the sense of
+            //that they are used when converting from Kilometers to Miles.
+            double distanceConst = 0; 
+            double fuelConst = 0;
+
+            double calculatedAverage = 0;
+            double distance = 0;
+            string parseDistance = "";
+
+            //Because the user might write the distance and end it with unit km or something else - we step by char and see if digit.
+            for (int i = 0; i < sDistance.Length; i++)
+            {
+                //If the selected character is not a digit and not a dot then it is one of the characters that is part of the selected unit of the user.
+                if (Char.IsDigit(sDistance.ToCharArray()[i]) || sDistance[i].Equals('.'))
+                {
+                    parseDistance += sDistance[i];
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            //Convert founded number value in sDistance to double (because it might be with decimals).
+            distance = Convert.ToDouble(parseDistance, System.Globalization.CultureInfo.InvariantCulture);
+
+            //Else-conditions if converting.
+            if (metrics.Equals("US"))
+            {
+                distanceConst = 1.6093472186944; //1 KM is about 1.6 miles
+                fuelConst = 3.78541178; //1 Liter is about 3.78 Gallons.
+                calculatedAverage = (distance / distanceConst) / (usedfuel / fuelConst);
+            }
+            else if (metrics.Equals("UK"))
+            {
+                distanceConst = 1.6093472186944; //1 KM is about 1.6 miles
+                fuelConst = 4.54609188; //1 Liter is about 3.78 Gallons.
+                calculatedAverage = (distance / distanceConst) / (usedfuel / fuelConst);
+            }
+            else if (metrics.Equals("EU"))
+            {
+                distanceConst = 1;
+                fuelConst = 1;
+                calculatedAverage = (usedfuel / (distance / 100));
+
+            }
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(
+                    "<string>"+calculatedAverage.ToString()+"</string>",
+                    System.Text.Encoding.UTF8,
+                    "text/xml"
+                )
+            };
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace DataNissen.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+           return View("home");
+        }
+
+        public ContentResult IsSession()
+        {
+            if (Session["clicked"] != null)
+            {
+                return Content("Session is set");
+            }
+            else
+            {
+                return Content("Session is unset");
+            }
+        }
+        
+        [HttpPost]
+        public ContentResult SetSession()
+        {
+            Session["clicked"] = "true";
+            return IsSession();
+        }
+
+        [HttpPost]
+        public ContentResult UnsetSession()
+        {
+            for (int i = 0; i < Session.Count; i++)
+            {
+                Session[i] = null;
+            }
+            return IsSession();
+        }
+
+        public ActionResult ProjectDescription(int numbers=0)
+        {
+            return View("project");     
+        }
+
+        [HttpPost]
+        public string ProjectDescription(string numbers = "0")
+        {
+            return numbers;
+        }
+
+    }
+}

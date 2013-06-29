0477626edd02270c8efad3705a1f2a034b1e3a72
Joel Mandell <joelmandell@gmail.com>
2013-06-29 22:46:44 +0300

    random backup, because will be wo my main dev machine

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,34 +17,5 @@ namespace DataNissen.Controllers
            return View("home");
         }
 
-        public ContentResult IsSession()
-        {
-            if (Session["clicked"] != null)
-            {
-                return Content("Session is set");
-            }
-            else
-            {
-                return Content("Session is unset");
-            }
-        }
-        
-        [HttpPost]
-        public ContentResult SetSession()
-        {
-            Session["clicked"] = "true";
-            return IsSession();
-        }
-
-        [HttpPost]
-        public ContentResult UnsetSession()
-        {
-            for (int i = 0; i < Session.Count; i++)
-            {
-                Session[i] = null;
-            }
-            return IsSession();
-        }
-
     }
 }

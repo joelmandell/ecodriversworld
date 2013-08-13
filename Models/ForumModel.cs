8abb1a031937a582495ac9825ffc9e05432dd83a
Joel Mandell <joelmandell@gmail.com>
2013-08-13 11:35:05 +0200

    initial code-first approach model forum

--- Models/ForumModel.cs
+++ Models/ForumModel.cs
@@ -5,7 +5,34 @@ using System.Web;
 
 namespace DataNissen.Models
 {
-    public class ForumModel
+    public class Subject
     {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public String Title { get; set; }
     }
+
+    public class Thread
+    {
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public Boolean Locked { get; set; }
+    }
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public int ThreadId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string Text { get; set; }
+    }
+
 }
